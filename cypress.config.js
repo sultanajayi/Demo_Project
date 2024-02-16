8f2d0edf2839a742d7160405dfd12c8d9bbe057a
Sultan Ajayi <hayobamidele39@gmail.com>
2024-02-16 12:35:37 +0100

    Login

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
+    defaultCommandTimeout: 10000,
+    experimentalWebKitSupport: true,
+    experimentalRunAllSpecs: true,
+    includeShadowDom: true,
+    chromeWebSecuritySupport: true,
+    viewportHeight: 800,
+    viewportWidth: 1500,
+    watchForFileChanges: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/assessment/login.spec.cy.js
+++ cypress/e2e/assessment/login.spec.cy.js
@@ -1,5 +1,36 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+let sel
+describe('', () => {
+  beforeEach(() => {
+    cy.visit("/", {failOnStatusCode: false});
+  });
+
+  before(() => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      return false
+    });
+    cy.fixture('selectors').then((selectors) => {
+      sel = selectors;
+    });
+  });
+
+  it('allows a user to log in with valid credentials', () => {
+      cy.get('sel.username').type('Admin')
+      cy.get('sel.password').type('admin123')
+      cy.get('sel.submitButton').click()
+
   })
+
+  it('displays an error message for invalid credentials', () => {
+    cy.get('sel.username').type('Admin')
+      cy.get('sel.password').type('admin1237888')
+      cy.get('sel.submitButton').click()
+      cy.contains('Invalid credentials')
+  });
+
+  it('displays an error message when required fields are left blank', () => {
+    cy.get('sel.submitButton').click()
+    cy.contains('Required')
+    cy.contains('Required')
+  });
+
 })
